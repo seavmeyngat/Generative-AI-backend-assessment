a616bd2960a84369796278e3b1eed14c2376aa05
seavmeyngat <seavmey.ngat@institute.pse.ngo>
2025-01-31 09:03:41 +0700

    last

--- src/routes/certificateRoute.ts
+++ src/routes/certificateRoute.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { createCertificate } from '../controllers/certificate.controller';
-
+import protectRoute from "../middleware/auth";
 const router = Router();
 
-router.post("/create", createCertificate);
+router.post("/create", protectRoute(), createCertificate);
 
 export default router;
